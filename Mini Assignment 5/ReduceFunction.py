d9a394917434a895bfd1456cf1425c4cd1aa3bdc
yashmittal2 <yashmittal2@deloitte.com>
2022-04-06 13:50:34 +0530

    Reduce Function Made to the point.

--- Mini Assignment 5/ReduceFunction.py
+++ Mini Assignment 5/ReduceFunction.py
@@ -3,7 +3,5 @@ from operator import mul
 from functools import reduce
 
 lst1 = [1, 2, 3, 4, 5, 6]
-ans1 = reduce(add, lst1)
-lst2 = [ans1, 2, 2]
-ans2 = reduce(mul, lst2)
+ans2 = reduce(mul, lst1)
 print(ans2)
